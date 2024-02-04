375bb1f6b442c68d05eef248c68558bf7b45666e
sehdevkumar <kumarsehdev600@gmail.com>
2024-02-04 22:59:10 +0530

    d3

--- src/typings/app-comman-typings.tsx
+++ src/typings/app-comman-typings.tsx
@@ -1,5 +1,10 @@
+import * as d3 from 'd3';
+
 export type HttpInitialState<T> = {
   data: T
   error: T
   isLoading: boolean
 }
+
+
+export type d3aseSelection = d3.Selection<any,any,any,any>;
--- src/views/intro-page.tsx
+++ src/views/intro-page.tsx
@@ -4,7 +4,7 @@ const IntroPage = () => {
   return (
     <>
       <div className="grid h-full  p-5 gap-x-10 grid-cols-2 justify-center items-center w-full">
-        <img src="../../public/assets/hacker.jpg" className="profile rounded-full w-60 h-60 shadow-orange-300 shadow-sm justify-self-end" />
+        <img src="../../public/assets/profile.png" className="profile rounded-full w-60 h-60 shadow-orange-300 shadow-sm justify-self-end object-cover" />
         <div className="intro  text-left flex flex-col gap-y-2">
            
            <div className="subtitle font-mono text-[23px]">HI, THERE !</div>
